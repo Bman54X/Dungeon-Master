ff317e289881cb8956e7a201465ec6a693c4b0f0
ChristopherAlly1 <chris123ally@outlook.com>
2017-01-29 14:44:46 -0500

    Added In 3 Number Code
    Text is visible through walls have to fix

---
+++ Assets/Scripts/ChangeNumber.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeNumber : MonoBehaviour {
+    int i = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void changeNumber()
+    {
+        GetComponent<TextMesh>().text = i.ToString();
+        i++;
+        if (i == 11)
+        {
+            i = 0;
+            GetComponent<TextMesh>().text = i.ToString();
+        }
+    }
+     void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "player" && Input.GetKeyDown("space"))
+        {
+            changeNumber();
+        }
+    }
+}
---
+++ Assets/Scripts/CheckCode.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCode : MonoBehaviour {
+    public GameObject N1;
+    public GameObject N2;
+    public GameObject N3;
+    // Use this for initialization
+    void Start () {
+        N1 = GameObject.Find("N1");
+        N2 = GameObject.Find("N2");
+        N3 = GameObject.Find("N3");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        checkCode();
+	}
+    void checkCode()
+    {
+        if (N1 != null)
+        {
+            if (N1.GetComponentInChildren<TextMesh>().text == 1.ToString() && N2.GetComponentInChildren<TextMesh>().text == 2.ToString() && N3.GetComponentInChildren<TextMesh>().text == 3.ToString())
+            {
+                Destroy(N1);
+            }
+        }
+    }
+}
