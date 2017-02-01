63ad4eb5265c94160c0a09a2041a21fb6fc0887a
Logan Tawse <zeldalog42@hotmail.com>
2017-01-31 23:05:29 -0500

    A bunch of animations working, fixed see through text

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class Character : MonoBehaviour {
     Vector3 moveDirection;
-    public float speed = 6.0f, jumpSpeed = 15.0f,
-                 rotateSpeed = 5.0f, gravity = 9.81f;
+    public float speed = 6.0f, jumpSpeed = 50.0f,
+				 rotateSpeed = 5.0f, gravity = 9.81f;
     CharacterController cc;
+    Animator anim;
+	bool waitToJump, jumpPressed, crouching, walking;
+	bool startWalkCounter;
+	int jumpCounter, walkCounter;
 
     // Use this for initialization
     void Start () {
@@ -14,29 +18,91 @@ public class Character : MonoBehaviour {
         if (cc == null) {
             Debug.Log("No CharacterController found.");
         }
+
+		anim = GetComponent<Animator>();
+		if (anim == null) {
+			Debug.Log("No Animator found.");
+		}
+
+		waitToJump = false; jumpPressed = false;
+		crouching = false; walking = false;
+		startWalkCounter = false;
+		jumpCounter = 0; walkCounter = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (cc.isGrounded) {
-            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
-            moveDirection *= speed;
+		if (cc.isGrounded && !jumpPressed) {
+			if (Input.GetButtonDown ("NormalAttack")) {
+				anim.SetTrigger ("NormalAttack");
+			} else if (Input.GetButtonDown ("StrongAttack")) {
+				anim.SetTrigger ("StrongAttack");
+			}  else if (Input.GetButtonDown ("Stab")) {
+				anim.SetTrigger ("Stab");
+			}
 
-            //Key Press Stuff
-            if (Input.GetButtonDown("Jump")) {
-                moveDirection.y = jumpSpeed;
-            }
-        }
+			moveDirection = new Vector3 (0, 0, Input.GetAxis ("Vertical"));
+			moveDirection = transform.TransformDirection (moveDirection);
+            
+			if (Input.GetButton ("Crouch") && !walking) {
+				anim.SetBool ("CrouchHeld", true);
+				crouching = true;
+			} else if (Input.GetButton ("Walk") && !crouching) {
+				anim.SetFloat ("Speed", 0.3f);
+				walking = true;
+			} else{
+				anim.SetBool ("CrouchHeld", false);
+				crouching = false; walking = false;
+			}
 
+			if (Input.GetButtonUp ("Walk")) {
+				startWalkCounter = true;
+			}
+
+			if (crouching || walking) {
+				moveDirection *= speed / 2.0f;
+			} else {
+				moveDirection *= speed;
+			}
+
+			//Key Press Stuff
+			if (Input.GetButtonDown ("Jump")) {
+				waitToJump = true;
+				jumpPressed = true;
+			}
+
+			if (startWalkCounter) {
+				walkCounter++;
+
+				if (walkCounter >= 20) {
+					walkCounter = 0;
+					startWalkCounter = false;
+				}
+			}
+
+			if (moveDirection.x == 0 && moveDirection.z == 0) {
+				anim.SetFloat ("Speed", 0);
+			} else if (Input.GetAxis("Vertical") < 0) {
+				anim.SetFloat ("Speed", -1);
+			} else if (!walking && !startWalkCounter) {
+				anim.SetFloat("Speed", 1);
+			}
+
+			anim.SetBool("Jump", false);
+		}
+
+		if (waitToJump) {
+			jumpCounter++;
+			anim.SetBool ("Jump", true);
+			if (jumpCounter == 20) {
+				jumpCounter = 0;
+				moveDirection.y = jumpSpeed;
+				jumpPressed = false; waitToJump = false;
+			}
+		}
+
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
         moveDirection.y -= gravity * Time.deltaTime;
         cc.Move(moveDirection * Time.deltaTime);
-
-        /*float y = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * 7.0f;
-
-        transform.Rotate(0, y, 0);
-        transform.Translate(0, moveDirection.y, z);*/
     }
 }
