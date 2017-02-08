56b211378e49a267093f3c7753d01e1d56677c84
Logan Tawse <zeldalog42@hotmail.com>
2017-02-08 00:02:20 -0500

    UI, Potions, Switching Weapons, Arrow Collectible

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class Character : MonoBehaviour {
+	public enum Potions { SPEED, HEALTH, DEFENSE, GOLD }
+
 	CharacterController cc;
 	Animator anim;
 	public Transform cameraTransform;
@@ -11,17 +14,28 @@ public class Character : MonoBehaviour {
 	Vector3 prevLoc, curLoc, forward, right;
 
 	bool waitToJump = false, jumpPressed = false, crouching = false, walking = false;
-	bool startWalkCounter = false;
+	bool startWalkCounter = false, crossbowFound = false, swordEquipped = true;
+	bool changedPotions;
 
-	int jumpCounter = 0, walkCounter = 0, holdAttack = 0;
+	const int maxHealth = 100, maxArrows = 30;
+	int jumpCounter = 0, walkCounter = 0, holdAttack = 0, potionCount = 0;
+	int bowAmmo = 0, bombAmmo = 0, gold = 0, _health;
 
-	const float maxHealth = 100;
-	public float speed = 6.0f, jumpSpeed = 50.0f,
-	rotateSpeed = 5.0f, gravity = 9.81f;
-	float gold = 0.0f, _health;
+	const float gravity = 9.81f;
+	float speed = 6.0f, jumpSpeed = 15.0f, rotateSpeed = 10.0f;
+
+	public Text healthText, goldText, addedGold, bowAmmoText;
+	public Slider healthSlider;
+
+	public GameObject backCrossbow, handCrossbow, backSword, handSword;
+	public GameObject swordIcon, bowIcon;
+
+	Potions potionEquipped = Potions.HEALTH;
+
+	Dictionary<Potions, int> potionInventory = new Dictionary<Potions, int>();
 
     // Use this for initialization
-    void Start () {
+    void Awake () {
         cc = GetComponent<CharacterController>();
         if (cc == null) {
             Debug.Log("No CharacterController found.");
@@ -34,22 +48,66 @@ public class Character : MonoBehaviour {
 
 		curLoc = transform.position;
 		_health = maxHealth;
+
+		healthSlider.value = _health;
+		healthText.text = _health.ToString();
+		goldText.text = "Gold: " + gold;
+		addedGold.text = "";
+		bowAmmoText.text = bowAmmo.ToString();
+
+		crossbowFound = true;
+
+		backCrossbow.SetActive (true); handCrossbow.SetActive (false);
+		backSword.SetActive (false); handSword.SetActive (true);
+		bowIcon.SetActive (false); swordIcon.SetActive (true);
+
+		potionInventory.Add (Potions.HEALTH, 0); potionInventory.Add (Potions.GOLD, 0);
+		potionInventory.Add (Potions.DEFENSE, 0); potionInventory.Add (Potions.SPEED, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		healthSlider.value = _health;
+		healthText.text = _health.ToString();
+		goldText.text = "Gold: " + gold;
+		bowAmmoText.text = bowAmmo.ToString();
+
 		if (cc.isGrounded && !jumpPressed) {
-			if (Input.GetButton ("NormalAttack")) {
+			if (Input.GetButton ("NormalAttack") && swordEquipped) {
 				holdAttack++;
-			} else if (Input.GetButtonUp ("NormalAttack")) {
+			} else if (Input.GetButtonUp ("NormalAttack") && swordEquipped) {
 				if (holdAttack >= 45) {
 					anim.SetTrigger ("StrongAttack");
 				} else {
 					anim.SetTrigger ("NormalAttack");
 				}
 				holdAttack = 0;
-			}  else if (Input.GetButtonDown ("Stab")) {
+			} else if (Input.GetButtonDown ("Stab") && swordEquipped) {
 				anim.SetTrigger ("Stab");
+			} else if (Input.GetButtonDown ("SwitchWeapon") && crossbowFound) {
+				anim.SetTrigger ("GetSword");
+				Invoke("switchWeapons", 0.4f);
+			}
+
+			if (potionCount > 0 && potionCount < 5) {
+				potionCount++;
+			} else if (potionCount >= 5) {
+				
+				potionCount = 0;
+			}
+			changedPotions = false;
+			if (Input.GetButtonDown("HealthPotion")) {
+				potionEquipped = Potions.HEALTH;
+				changedPotions = true; potionCount++;
+			} else if (Input.GetButtonDown("DefensePotion")) {
+				potionEquipped = Potions.DEFENSE;
+				changedPotions = true; potionCount++;
+			} else if (Input.GetButtonDown("SpeedPotion")) {
+				potionEquipped = Potions.SPEED;
+				changedPotions = true; potionCount++;
+			} else if (Input.GetButtonDown("GoldPotion")) {
+				potionEquipped = Potions.GOLD;
+				changedPotions = true; potionCount++;
 			}
 
 			float x = Input.GetAxis ("Horizontal");
@@ -132,9 +190,21 @@ public class Character : MonoBehaviour {
         cc.Move(moveDirection * Time.deltaTime);
     }
 
-	public float health {
+	public int health {
 		get { return _health; }
 		set { _health = value; }
+	}
+
+	public bool getChangedPotions() {
+		return changedPotions;
+	}
+
+	public Potions getPotion() {
+		return potionEquipped;
+	}
+
+	public int getPotionAmount() {
+		return potionInventory[potionEquipped];
 	}
 
 	void OnTriggerEnter(Collider other) {
@@ -148,9 +218,43 @@ public class Character : MonoBehaviour {
 
 			Destroy (other.gameObject);
 		} else if (other.gameObject.CompareTag("Gold")) {
-			gold += Random.Range (1, 21);
+			int chance = Random.Range (1, 11);
+			if (chance <= 8) {
+				gold += Random.Range (5, 51);
+			} else {
+				gold += Random.Range (51, 100);
+			}
+			addedGold.text = "+" + gold.ToString ();
+			Invoke("resetGoldText", 1.5f);
+
+			Destroy (other.gameObject);
+		}  else if (other.gameObject.CompareTag("ArrowBundle")) {
+			if (bowAmmo < maxArrows) {
+				bowAmmo += 5;
+				if (bowAmmo > maxArrows) {
+					bowAmmo = maxArrows;
+				}
+			}
 
 			Destroy (other.gameObject);
 		}
-	}	
+	}
+
+	void resetGoldText() {
+		addedGold.text = "";
+	}
+
+	void switchWeapons() {
+		if (swordEquipped) {
+			backCrossbow.SetActive (false); handCrossbow.SetActive (true);
+			backSword.SetActive (true); handSword.SetActive (false);
+			bowIcon.SetActive (true); swordIcon.SetActive (false);
+		} else {
+			backCrossbow.SetActive (true); handCrossbow.SetActive (false);
+			backSword.SetActive (false); handSword.SetActive (true);
+			bowIcon.SetActive (false); swordIcon.SetActive (true);
+		}
+
+		swordEquipped = !swordEquipped;
+	}
 }
---
+++ Assets/Scripts/PotionUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotionUI : MonoBehaviour {
+	public Character player;
+	Character.Potions currentPotion, previousPotion;
+	int potionAmount;
+
+	public GameObject[] potions = new GameObject[4];
+
+	Dictionary<Character.Potions, GameObject> potionImages = new Dictionary<Character.Potions, GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		currentPotion = player.getPotion ();
+		potionAmount = player.getPotionAmount ();
+		previousPotion = currentPotion;
+
+		potionImages.Add (Character.Potions.HEALTH, potions[0]); potionImages.Add (Character.Potions.DEFENSE, potions[1]); 
+		potionImages.Add (Character.Potions.SPEED, potions[2]); potionImages.Add (Character.Potions.GOLD, potions[3]);
+
+		for (int i = 0; i < 4; i++) {
+			potions [i].SetActive (false);
+		}
+
+		potionImages [currentPotion].SetActive (true);
+	}
+
+	void Update() {
+		if (player.getChangedPotions()) {
+			changedPotions (player.getPotion(), player.getPotionAmount());
+		}
+	}
+	
+	void changedPotions(Character.Potions newPotion, int numPotions) {
+		if (newPotion != currentPotion) {
+			currentPotion = newPotion; potionAmount = numPotions;
+			potionImages [currentPotion].SetActive (true);
+			potionImages [previousPotion].SetActive (false);
+			previousPotion = currentPotion;
+		}
+	}
+}
