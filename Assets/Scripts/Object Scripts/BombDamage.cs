2de0aa6d28feb87e5fb82eb0a082e4ba02ac9f85
Logan Tawse <zeldalog42@hotmail.com>
2017-03-19 18:07:10 -0400

    Stuff
    -added wood texture to doors
    -added 3 doors
    -added 2 spawn points
    -added/edited various walls and their textures
    -linked up rooms

---
+++ Assets/Scripts/Object Scripts/BombDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombDamage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider other)
+    {
+       // Debug.Log("stay trigger");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+     //   Debug.Log("trigger enter");
+    }
+}
---
+++ Assets/Scripts/VR Scripts/SpawnNewRockCrossbow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnNewRockCrossbow : MonoBehaviour
+{
+    public float rockRespawnTime = 3.0f;
+    public bool rock = false;
+    public GameObject rockPrefab;
+    public Transform rockSpawnPoint;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()  {
+        if (rock == true){
+            rockRespawnTime = rockRespawnTime - Time.deltaTime;
+            if (rockRespawnTime <= 0){
+                //spawn new bomb
+                GameObject clone = Instantiate(rockPrefab, rockSpawnPoint.position, Quaternion.identity);
+                //  clone.transform.parent = vrThrowingplat.transform;
+                rock = false;
+                rockRespawnTime = 3.0f;
+            }
+        }
+    }
+}
